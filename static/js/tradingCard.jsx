4f3325022d552cb24de318ac44330bf747891f22
shoshlavosh <shoshlavosh@gmail.com>
2021-08-05 23:33:23 +0000

    Lab step 4 partially complete

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -1,11 +1,54 @@
 "use strict";
 const tradingCardData = [
   {
-    name: 'Balloonicorn',
-    skill: 'video games',
-    imgUrl: '/static/img/balloonicorn.jpg'
-  }
-]
+    name: "Balloonicorn",
+    skill: "video games",
+    imgUrl: "/static/img/balloonicorn.jpg",
+    cardId: 1,
+  },
+  {
+    name: "Float",
+    skill: "baking pretzels",
+    imgUrl: "/static/img/float.jpg",
+    cardId: 2,
+  },
+  {
+    name: "Llambda",
+    skill: "knitting scarves",
+    imgUrl: "/static/img/llambda.jpg",
+    cardId: 3,
+  },
+  {
+    name: "Off-By-One",
+    skill: "climbing mountains",
+    imgUrl: "/static/img/off-by-one.jpeg",
+    cardId: 4,
+  },
+  {
+    name: "Seed.py",
+    skill: "making curry dishes",
+    imgUrl: "/static/img/seedpy.jpeg",
+    cardId: 5,
+  },
+  {
+    name: "Polymorphism",
+    skill: "costumes",
+    imgUrl: "/static/img/polymorphism.jpeg",
+    cardId: 6,
+  },
+  {
+    name: "Short Stack Overflow",
+    skill: "ocean animal trivia",
+    imgUrl: "/static/img/shortstack-overflow.jpeg",
+    cardId: 7,
+  },
+  {
+    name: "Merge",
+    skill: "bullet journaling",
+    imgUrl: "/static/img/merge.png",
+    cardId: 8,
+  },
+];
 
 function TradingCard(props) {
   return (
@@ -55,7 +98,7 @@ ReactDOM.render(
     <TradingCard
       name="Banana"
       skill="slipping"
-      imgURL="/static/img/merge.png"
+      imgUrl="/static/img/merge.png"
     />
   ),
   document.querySelector('#banana')
@@ -71,3 +114,4 @@ ReactDOM.render(
   ),
   document.querySelector('#strawberry')
 );
+
